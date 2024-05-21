79d5f433470ab33584bcf57a4a0db820da885bd0
MKazmierczak02 <mmichal.kkazmierczak@gmail.com>
2024-05-22 01:16:02 +0200

    Add styling

--- backend/static/js/home.js
+++ backend/static/js/home.js
@@ -1,13 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
 const observer = new IntersectionObserver((entries) => {
-  entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      entry.target.classList.add("visible");
-      observer.unobserve(entry.target);
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('visible');
+      observer.unobserve(entry.target)
+    } else {
+      entry.target.classList.remove('visible');
     }
-  });
-});
+  })
+})
 
-const howItWorksContainer = document.querySelector(".how-it-works-container");
-observer.observe(howItWorksContainer);
-});
+const hiddenElements = document.querySelectorAll(".hidden");
+
+console.log(hiddenElements);
+hiddenElements.forEach(el => observer.observe(el))
