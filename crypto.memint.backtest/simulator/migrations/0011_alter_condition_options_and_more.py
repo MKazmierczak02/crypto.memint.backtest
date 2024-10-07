a6dbe0b65db9eac44940cb60c8c46e1fd254412d
MKazmierczak02 <mmichal.kkazmierczak@gmail.com>
2024-10-08 00:14:34 +0200

    apply new migrations

---
+++ crypto.memint.backtest/simulator/migrations/0011_alter_condition_options_and_more.py
@@ -0,0 +1,34 @@
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('simulator', '0010_remove_position_initial_size'),
+    ]
+
+    operations = [
+        migrations.AlterModelOptions(
+            name='condition',
+            options={'ordering': ['order']},
+        ),
+        migrations.RemoveField(
+            model_name='strategy',
+            name='max_positions',
+        ),
+        migrations.AddField(
+            model_name='simulation',
+            name='max_positions',
+            field=models.PositiveSmallIntegerField(default=1),
+        ),
+        migrations.AlterField(
+            model_name='position',
+            name='entry_price',
+            field=models.DecimalField(decimal_places=10, max_digits=20),
+        ),
+        migrations.AlterField(
+            model_name='position',
+            name='size',
+            field=models.DecimalField(decimal_places=10, max_digits=20),
+        ),
+    ]
