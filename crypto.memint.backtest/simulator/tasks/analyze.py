9d0045e71b20638fbc6c142661351f8a0e7b2be9
MKazmierczak02 <mmichal.kkazmierczak@gmail.com>
2024-10-08 00:15:53 +0200

    minimalize task

--- crypto.memint.backtest/simulator/tasks/analyze.py
+++ crypto.memint.backtest/simulator/tasks/analyze.py
@@ -1,20 +1,9 @@
-import datetime
-
 from celery import shared_task
 
-from ..handlers import MovingAverageCrossoverStrategy
-from ..models import PriceData, Simulation
+from simulator.models import Simulation
 
 
 @shared_task
 def analyze_market_data(simulation_id: int):
     simulation = Simulation.objects.get(id=simulation_id)
-    simulation_task = MovingAverageCrossoverStrategy(simulation)
-    # simulation.status = "Running"
-    # simulation.save()
-
-    simulation_task.run()
-
-    # simulation.end_date = datetime.datetime.now()
-    # simulation.status = "Finished"
-    # simulation.save()
+    simulation.run()
