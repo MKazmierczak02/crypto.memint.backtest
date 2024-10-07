489692576732a5a755cfdc90acec99cc1b55ba78
MKazmierczak02 <mmichal.kkazmierczak@gmail.com>
2024-10-08 00:17:14 +0200

    register new urls and admin views

--- crypto.memint.backtest/simulator/admin.py
+++ crypto.memint.backtest/simulator/admin.py
@@ -1,11 +1,29 @@
 from django.contrib import admin
-from simulator.models import (Condition, PriceData, Simulation,
-                              Strategy, Symbol, TimeFrame, Transaction)
 
+from .models import (
+    Condition,
+    Position,
+    PriceData,
+    Simulation,
+    Strategy,
+    Symbol,
+    TimeFrame,
+    SimulationResult,
+    ConditionGroup,
+    Action,
+    TechnicalIndicator,
+    Operand
+)
+
+admin.site.register(Position)
 admin.site.register(Simulation)
-admin.site.register(Symbol)
+admin.site.register(SimulationResult)
 admin.site.register(Strategy)
-admin.site.register(PriceData)
-admin.site.register(Condition)
-admin.site.register(Transaction)
+admin.site.register(TechnicalIndicator)
 admin.site.register(TimeFrame)
+admin.site.register(PriceData)
+admin.site.register(Symbol)
+admin.site.register(ConditionGroup)
+admin.site.register(Action)
+admin.site.register(Condition)
+admin.site.register(Operand)
--- crypto.memint.backtest/simulator/urls.py
+++ crypto.memint.backtest/simulator/urls.py
@@ -1,18 +1,43 @@
 from django.urls import include, path
 from rest_framework.routers import SimpleRouter
 
-from .views import (PriceViewSet, SimulationViewSet, StrategyViewSet,
-                    SymbolViewSet, check_analysis_result, start_simulation)
+from simulator.views import (
+    PriceViewSet,
+    SimulationViewSet,
+    StrategyViewSet,
+    SymbolViewSet,
+    TimeFrameViewSet,
+    check_analysis_result,
+    export_simulation_to_csv,
+    start_simulation,
+    reset_simulation,
+    simulation_summary
+)
+from simulator.views.utils import (
+    TechnicalIndicatorListView,
+    ActionListView,
+    OperandTypesListView
+)
 
 router = SimpleRouter()
 router.register(r"strategies", StrategyViewSet, basename="strategies")
-router.register(r"pairs", SymbolViewSet, basename="symbols")
+router.register(r"symbols", SymbolViewSet, basename="symbols")
 router.register(r"simulations", SimulationViewSet, basename="simulations")
 router.register(r"prices", PriceViewSet, basename="prices")
+router.register(r"timeframes", TimeFrameViewSet, basename="timeframes")
+
+util_urls = [
+    path('technical-indicators/', TechnicalIndicatorListView.as_view(), name='technical-indicator-list'),
+    path('actions/', ActionListView.as_view(), name='action-list'),
+    path('operand-types/', OperandTypesListView.as_view(), name='operand-types-list'),
+]
 
 
 urlpatterns = [
     path("start", start_simulation, name="start-simulation"),
     path("check", check_analysis_result, name="check-simulation"),
+    path("reset", reset_simulation, name="reset-simulation"),
+    path('summary/<int:simulation_id>/', simulation_summary, name='simulation-summary'),
+    path("report/<int:simulation_id>/", export_simulation_to_csv, name="export-simulation-csv"),
     path("", include(router.urls)),
-]
+] + util_urls
